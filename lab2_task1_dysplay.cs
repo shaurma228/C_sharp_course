5789e3ab99eb9cb1507eea15f545b1433fb208e0
shaurma228 <61076505+shaurma228@users.noreply.github.com>
2024-10-30 14:10:37 +0500

    Update lab2_task1_dysplay.cs

--- lab2_task1_dysplay.cs
+++ lab2_task1_dysplay.cs
@@ -1,46 +1,53 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class Display : Screen
+namespace lab2_task1
 {
-    string type; //display type (OLED, IPS etc.)
-    int refR; //refresh rate
-
-    public Display(int resH, int resV, int size, string type, int refR) : base(resH, resV, size)
+    class Display : Screen
     {
-        this.type = type;
-        this.refR = refR;
-    }
+        string type; //display type (OLED, IPS etc.)
+        int refR; //refresh rate
 
-    public string Type
-    {
-        get { return type; }
-    }
-
-    public int RefreshRate
-    {
-        get { return refR; }
-    }
-
-    public string Ratio()
-    {
-        int a = resV, b = resH;
-        int temp;
-        while (b != 0)
+        public Display(int resH, int resV, int size, string type, int refR) : base(resH, resV, size)
         {
-            temp = b;
-            b = a % b;
-            a = temp;
+            this.type = type;
+            this.refR = refR;
         }
-        return $"{resH / a}:{resV / a}";
-    }
 
-    public double PPI()
-    {
-        return Math.Round(Math.Sqrt(resV * resV + resH * resH) / size, 1);
-    }
+        public string Type
+        {
+            get { return type; }
+        }
 
-    public override string ToString()
-    {
-        return base.ToString() + $", Type: {type}, Refresh rate: {refR}HZ";
+        public int RefreshRate
+        {
+            get { return refR; }
+        }
+
+        public string Ratio()
+        {
+            int a = resV, b = resH;
+            int temp;
+            while (b != 0)
+            {
+                temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return $"{resH / a}:{resV / a}";
+        }
+
+        public double PPI()
+        {
+            return Math.Round(Math.Sqrt(resV * resV + resH * resH) / size, 1);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Type: {type}, Refresh rate: {refR}HZ";
+        }
     }
 }
