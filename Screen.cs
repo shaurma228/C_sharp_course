f71364209df7f1cb2e04ac2c300c8355981f6da5
shaurma228 <61076505+shaurma228@users.noreply.github.com>
2024-10-30 13:48:56 +0500

    Add files via upload

---
+++ Screen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Screen
+{
+    protected int resH, resV, size; //resolution horisontal, resolution vertical, size (inch)
+
+    public Screen(int a, int b, int c)
+    {
+        this.resH = a;
+        this.resV = b;
+        this.size = c;
+    }
+
+    public int MaxLastNum()
+    {
+        int last_a = Math.Abs(resH) % 10;
+        int last_b = Math.Abs(resV) % 10;
+        int last_c = Math.Abs(size) % 10;
+        return Math.Max(last_a, Math.Max(last_b, last_c));
+    }
+
+    public override string ToString()
+    {
+        return $"Resolution: {resH}x{resV}, Size: {size} inch";
+    }
+
+    public int A
+    {
+        get { return resH; }
+    }
+
+    public int B
+    {
+        get { return resV; }
+    }
+
+    public int C
+    {
+        get { return size; }
+    }
+}
